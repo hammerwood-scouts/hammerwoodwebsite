8e5bf680df9ec7414187854e4ec2d41e0704a1ea
Simon Legg <leggsimon@gmail.com>
2021-03-17 14:47:34 +0100

    add sass sourcemaps

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,6 +1,7 @@
 const { dest, src } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const sassProcessor = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
 // We want to be using canonical Sass, rather than node-sass
 sassProcessor.compiler = require('sass');
@@ -12,9 +13,11 @@ const isProduction = process.env.NODE_ENV === 'production';
 // processes them, then sends them to the output calculator
 const sass = () => {
 	return src('scss/*.scss')
+		.pipe(sourcemaps.init())
 		.pipe(sassProcessor().on('error', sassProcessor.logError))
 		.pipe(cleanCSS(isProduction ? { level: 2 } : {}))
-		.pipe(dest('dist', { sourceMaps: !isProduction }));
+		.pipe(sourcemaps.write('maps'))
+		.pipe(dest('dist', { sourceMaps: true }));
 };
 
 module.exports = sass;
