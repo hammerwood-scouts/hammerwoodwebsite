c330feee0251fcd5d288e44840cd34fb729a5cd7
Simon Legg <leggsimon@gmail.com>
2021-03-17 14:39:29 +0100

    add sass build step

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const { watch, parallel } = require('gulp');
 // Pull in each task
 const templates = require('./gulp/templates');
 const images = require('./gulp/images');
+const sass = require('./gulp/sass');
 
 // Set each directory and contents that we want to watch and
 // assign the relevant task. `ignoreInitial` set to true will
@@ -10,9 +11,11 @@ const images = require('./gulp/images');
 // will run when a file changes.
 const watcher = () => {
 	watch('./{layouts,pages}/**/*.html', { ignoreInitial: true }, templates);
+	watch('./scss/*.scss', { ignoreInitial: true }, sass);
 };
 
-exports.default = parallel(templates, images);
+exports.default = parallel(templates, images, sass);
 exports.templates = templates;
 exports.images = images;
+exports.sass = sass;
 exports.watch = watcher;
