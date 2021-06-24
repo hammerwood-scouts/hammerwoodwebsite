bca3962eade72155a81aa1819a5b5d10d6f04f8f
Simon Legg <leggsimon@gmail.com>
2021-06-24 16:44:01 +0200

    content body expands to fill vertical space

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -2,13 +2,14 @@ import { Announcement } from '../Announcement/Announcement';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
 import { Navigation } from '../Navigation/Navigation';
+import styles from './Layout.module.css';
 
 export const Layout = (props) => {
 	return (
 		<>
 			<Header />
 			<Navigation />
-			<main>
+			<main className={styles.main}>
 				<Announcement />
 				{props.children}
 			</main>
