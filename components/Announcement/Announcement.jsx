f1708f351441a5244fd0c96f50f7fa64fb2db38d
Simon Legg <leggsimon@gmail.com>
2021-07-06 15:35:54 +0200

    update announcement message

--- components/Announcement/Announcement.jsx
+++ components/Announcement/Announcement.jsx
@@ -1,4 +1,5 @@
 import styles from './Announcement.module.css';
+import Link from 'next/link';
 
 export function Announcement() {
 	// This announcement is displayed before the main content. It can be displayed in one of 3 ways which can be changed based on the classes added.
@@ -6,8 +7,12 @@ export function Announcement() {
 	// Adding the “orange” class will show an orange border and background for more warning type messages (i.e. “Booking are almost full.”)
 	// Adding the “red” class will show a red border and background for more severe type messages (i.e. “The site is closed.”)
 	return (
-		<div className={[styles.announcement, styles.red].join(' ')}>
-			Notice: The campsite is currently closed due to COVID-19 restrictions.
+		<div className={[styles.announcement, styles.orange].join(' ')}>
+			Campsite now open. Limited capacity.{' '}
+			<Link href="/bookings">
+				<a>Contact us</a>
+			</Link>{' '}
+			to check availability and book.
 		</div>
 	);
 }
